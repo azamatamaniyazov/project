ea704a151129f31d54ebf609c37f6362dc98a0e8
azaamaniyazov <azaamaniyazov@mail.ru>
2021-09-18 17:50:23 +0500

    added functions

--- script.js
+++ script.js
@@ -20,29 +20,46 @@ const personalMovieDB = {
   private: false,
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt("En aqirgi korgen kino?"),
-    b = prompt("Neshege bahalaysiz?");
+function showSecondQ() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("En aqirgi korgen kino?"),
+      b = prompt("Neshege bahalaysiz?");
 
-  if (a != null && b != null && a != "" && b != "" && a.length < 20) {
-    personalMovieDB.movies[a] = b;
-    console.log("Done");
-  } else {
-    console.log("Error");
-    i--;
+    if (a != null && b != null && a != "" && b != "" && a.length < 20) {
+      personalMovieDB.movies[a] = b;
+      console.log("Done");
+    } else {
+      console.log("Error");
+      i--;
+    }
   }
 }
 
-if (personalMovieDB.count < 10) {
-  alert("Az kino koripsiz");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  alert("Siz jaqsi korermen ekensiz");
-} else if (personalMovieDB.count >= 30) {
-  alert("Kinoman");
-} else alert("Qatelik");
+showSecondQ();
 
-function showMyDB(x) {
-  if (x == false) {
+function statMovie() {
+  if (personalMovieDB.count < 10) {
+    alert("Az kino koripsiz");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Siz jaqsi korermen ekensiz");
+  } else if (personalMovieDB.count >= 30) {
+    alert("Kinoman");
+  } else alert("Qatelik");
+}
+
+statMovie();
+
+function writeYourGenres() {
+  for (let i = 0; i < 3; i++) {
+    const genre = prompt(`Siz suygen janr ${i + 1}`, "");
+    personalMovieDB.genres[i] = genre;
+  }
+}
+
+writeYourGenres();
+
+function showMyDB(hidden) {
+  if (!hidden) {
     console.log(personalMovieDB);
   } else console.log("Data base private");
 }
